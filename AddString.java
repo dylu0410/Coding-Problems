fff0da731a18e6b6fd6fd4b9337a5835a824a56a
Christina Lu <lu.1774@osu.edu>
2016-10-29 22:37:00 -0400

    AddSting.class
    (1) The previous one can not deal with overflow successfully
    (2) Pay attention to carry bit

---
+++ AddString.java
@@ -0,0 +1,68 @@
+package Math;
+
+public class AddString {
+	
+	public String addStrings(String num1, String num2)
+	{
+		//if (s1==""|| s2=="") return
+		//overflow:
+		/*int len1= s1.length();
+		int len2= s2.length();
+		int len=(len1> len2)? len1: len2;
+		int []result= new int[len+1];
+		
+		//assume both positive
+		//4099+22
+		String rev_s1= new StringBuilder(s1).reverse().toString();  //reverse the string
+		String rev_s2= new StringBuilder(s2).reverse().toString();
+		for (int i=0; i<len+1; i++)
+		{	
+			int s1_num= i>=len1?0: (int)(rev_s1.charAt(i)-'0');   //char to int
+			int s2_num= i>=len2?0:(int)(rev_s2.charAt(i)-'0');
+			result[i]+= s1_num+s2_num;
+			if (result[i]>=10 && i<len)
+			{
+				result[i+1]+= result[i]/10;     //deal with carr bit
+				result[i]%=10;
+			}
+			System.out.println(i+" "+result[i]);
+		}
+		//for (int i)
+		StringBuilder sb= new StringBuilder();
+		for (int i=len; i>=0; i--)
+		{
+			boolean useless_zero= true;    //if the first bit is 0
+			if (result[i]==0 && useless_zero) continue;
+			else
+			{	
+				useless_zero=false;
+				sb.append(result[i]);
+			}
+		}
+		return !sb.toString().equals("")?sb.toString():"0";*/
+		//solution2:
+		int i= num1.length()-1; int j= num2.length()-1;
+		int sum=0, carry=0;
+		StringBuilder sb= new StringBuilder();
+		while (i>=0 || j>=0)
+		{
+			int n1= i<0?0:(int)(num1.charAt(i)-'0');
+			int n2= j<0?0:(int)(num2.charAt(j)-'0');
+			sum= n1+n2+carry;
+			carry= sum/10;
+			sum= sum%10;
+			sb.append(sum);
+			i--; j--;
+		}
+		return sb.reverse().toString();
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		AddString test= new AddString();
+		System.out.println(1099+20099);
+		System.out.println(test.addStrings("12345", "9"));
+	}
+
+}
